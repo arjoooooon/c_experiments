8c12be114a16b7e660aa1345afb6db0a96646490
Arjun Taneja <arjun.taneja02@gmail.com>
2020-11-30 19:42:35 +0800

    Functioning base conversion utility

--- baseconv/main.c
+++ baseconv/main.c
@@ -1,37 +1,81 @@
-#include <string.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int characterReference[256];
+int characterReference[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-void constructCharacterReference(){
-    for(int i = 0; i < 10; i++){
-        characterReference[i] = '0' + i;
+int numericalReference[256];
+
+void usage(){
+    printf("Usage: ./main [-h] input_base output_base input_value\n");
+}
+
+void construct_numerical_reference(){
+    for(int c = '0', i = 0; c <= '9'; c++, i++){
+        numericalReference[c] = i;
     }
-    for(char c = 'A'; c < 'G'; c++){
-        characterReference[c] = c - 55;
+    for(int c = 'A', i = 10; c <= 'F'; c++, i++){
+        numericalReference[c] = i;
     }
 }
 
-int main(int argc, char** argv){
-    constructCharacterReference();
-    if(strcmp(argv[1], "--help") == 0) {
-        printf("USAGE:\n"
-            "--help to get help docs for program\n"
-            "./main [input] [input_base] [target_base]\n"
-            "[input] = input string with number from any base up til hexadecimal\n"
-            "[input_base] = base of input\n"
-            "[target_base] = base of output\n");
-    } else if(argc != 3) {
-        printf("Three input arguments required. Use flag --help to read documentation.\n");
-        return 1;
+void swap(char* str) {
+    int len = strlen(str);
+    for(int i = 0; i < len/2; i++){
+        char temp = str[i];
+        str[i] = str[len-1-i];
+        str[len-1-i] = temp;
+    }
+}
+
+uint64_t conv_to_decimal(int base, char* input_string) {
+    uint64_t modifier = 1;
+    uint64_t ret = 0;
+    int len = strlen(input_string);
+    
+    for(int i = len-1; i > -1; i--) {
+        ret += modifier * numericalReference[input_string[i] - 0];
+        modifier *= base;
     }
 
-    char* input = argv[1];
-    char* base = argv[2];
-    char* targetBase = argv[3];
+    return ret;
+}
 
-    int inputBase = atoi(base);
+char* conv_to_base(int base, uint64_t value) {
+    char* output_string = (char*)malloc(sizeof(char) * 256);
+    int iter = 0;
+    
+    while(value >= base) {
+        output_string[iter] = characterReference[value % base];
+        value /= base;
+        iter++;
+    }
+    output_string[iter] = characterReference[value];
+    output_string[iter + 1] = '\0';
+
+    swap(output_string);
+
+    return output_string;
+}
+
+int main(int argc, char** argv){
+    if(argc == 1) { usage(); return 1; }
+    if(strcmp(argv[1], "-h") == 0) { usage(); return 0; }
+    if(argc != 4) { usage(); return 1; }
+    
+    construct_numerical_reference();
+
+    int input_base = atoi(argv[1]);
+    int output_base = atoi(argv[2]);
+    char* input_string = argv[3];
+    
+    uint64_t input_value = conv_to_decimal(input_base, input_string);
+    
+    char* output_string = conv_to_base(output_base, input_value);
+    printf("%s\n", output_string);
+
+    free(output_string);
 
     return 0;
 }
--- hash/main.c
+++
@@ -1,9 +0,0 @@
-#include "hash.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char** argv){
-    uint16_t  b = hash("aa");
-
-    return 0;
-}
